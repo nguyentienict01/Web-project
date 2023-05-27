4a8ae487103b19b0cc1ca61e891e5daad16f764e
thinh205171 <75664882+thinh205171@users.noreply.github.com>
2023-05-28 00:11:38 +0700

    Login + SignUp function

--- api/index.js
+++ api/index.js
@@ -1,17 +1,80 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+const User = require("./models/User.js");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken")
+const app = express();
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = "abcabcabcabcabc"
+const CookieParser = require("cookie-parser")
 
-app.use(express.json())
+require("dotenv").config();
+app.use(express.json());
+app.use(CookieParser())
+app.use(
+    cors({
+        credentials: true,
+        origin: "http://localhost:5173",
+    })
+);
 
-app.use(cors({
-    credentials: true,
-    origin: 'https://127.0.0.1:5173'
-}))
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("connect start")).catch(console.error())
 
-app.post('/register', (req, res) => {
-    const {name, email, password} = req.body
-    res.json(name, email, password)
+
+app.get("/test", (req, res) => {
+    res.json("test okay");
+});
+
+app.post("/register", async (req, res) => {
+    const { name, email, password } = req.body;
+    // res.json({name, email, password})
+    try {
+        const userDoc = await User.create({
+            name,
+            email,
+            password: bcrypt.hashSync(password, bcryptSalt),
+            password,
+        });
+        res.json(userDoc);
+    } catch (error) {
+        res.status(422).json(error);
+    }
+});
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body
+    const userDoc = await User.findOne({ email })
+    if (userDoc) {
+        const pass_check = bcrypt.compareSync(password, userDoc.password)
+        if (pass_check) {
+            jwt.sign({ email: userDoc.email, id: userDoc._id, name: userDoc.name }, jwtSecret, {}, (error, token) => {
+                if (error)
+                    throw error
+                res.cookie('token', token).json(userDoc)
+            })
+        }
+        else
+            res.json("wrong pass")
+    }
+    else
+        res.json('not found')
 })
 
-app.listen(8000)
+app.get('/profile', (req, res) => {
+    const { token } = req.cookies
+    if (token) {
+        jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+            if (err) throw err
+            const {name, email, _id} = await User.findById(userData.id)
+            res.json({name, email, _id})
+        })
+    }
+    else
+        res.json(null)
+})
+
+app.listen(8000);
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -3,20 +3,25 @@ import { Link } from 'react-router-dom'
 import axios from 'axios'
 
 const RegisterPage = () => {
-    const [name, setName] = useState();
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    function registerUser(ev) {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    async function registerUser(ev) {
         ev.preventDefault();
-        axios.post('/register', {
-            name, email, password
-        });
+        try {
+            await axios.post('/register', {
+                name, email, password
+            });
+            alert("Successful registration!")
+        } catch (error) {
+            alert("Registration failed. Please try again")
+        }
     }
     return (
         <div className='mt-4 grow flex items-center justify-around'>
             <div className="mb-64">
-                <h1 className='text-4xl text-center'>Login</h1>
-                <form action="" className='max-w-md mx-auto' onSubmit={registerUser}>
+                <h1 className='text-4xl text-center'>Register Account</h1>
+                <form action="" className='max-w-md mx-auto mt-5' onSubmit={registerUser}>
                     <input type="text" placeholder='Name' value={name} onChange={ev => setName(ev.target.value)} />
                     <input type="email" placeholder='Email' value={email} onChange={ev => setEmail(ev.target.value)}/>
                     <input type="password" placeholder='Password' value={password} onChange={ev => setPassword(ev.target.value)}/>
