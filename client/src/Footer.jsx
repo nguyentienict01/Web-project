368d14db91d17c7e764163c5009ab22357cb5dad
thinh205171 <75664882+thinh205171@users.noreply.github.com>
2023-06-20 18:41:45 +0700

    hiiiiiiii

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div className="bg-gray-200 p-4">Footer</div>
-  )
+    return (
+        <div className="bg-gray-200 left-0 z-20 w-full border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Web Technology & E-Service</a>.</span>
+            <span class="text-2xl">Group 21</span>
+            <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6">About</a>
+                </li>
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </div>
+
+    )
 }
 
 export default Footer
--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -5,13 +5,24 @@ import { Outlet } from "react-router-dom";
 export default function Layout() {
   return (
 
-    <div className="py-4 px-8 flex flex-col min-h-screen max-w-4xl mx-auto">
-      <div className="flex-grow">
-        <Header />
-        <Outlet />
-        <Footer />
+    <>
+      <div className="min-h-screen mx-auto">
+        <div className="px-8 flex flex-col max-w-4xl mx-auto">
+          <div className="sticky top-0">
+            <Header />
+          </div>
+          <div className="">
+            <Outlet />
+          </div>
+          <div style={{ height: "1000px" }}></div>
+        </div>
+        
+        <div className="bottom-0 sticky">
+          <Footer />
+        </div>
       </div>
 
-    </div>
+
+    </>
   );
 }
