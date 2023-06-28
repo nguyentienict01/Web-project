ab942d3b867c9d00c2372368b40ce0355b4efcbb
Luat <you@example.com>
2023-06-28 14:32:32 +0700

    Update loginpage style

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,47 +1,60 @@
-import {Link, Navigate} from "react-router-dom";
-import {useContext, useState} from "react";
+import { Link, Navigate } from "react-router-dom";
+import { useContext, useState } from "react";
 import axios from "axios";
-import {UserContext} from "../UserContext.jsx";
+import { UserContext } from "../UserContext.jsx";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [redirect, setRedirect] = useState(false);
-  const {setUser} = useContext(UserContext);
+  const { setUser } = useContext(UserContext);
   async function handleLoginSubmit(ev) {
     ev.preventDefault();
     try {
-      const {data} = await axios.post('http://localhost:4000/login', {email,password});
-      setUser(data);  
-      alert('Login successful');
+      const { data } = await axios.post("http://localhost:4000/login", {
+        email,
+        password,
+      });
+      setUser(data);
+      alert("Login successful");
       setRedirect(true);
     } catch (e) {
-      alert('Login failed');
+      alert("Login failed");
     }
   }
 
   if (redirect) {
-    return <Navigate to={'/'} />
+    return <Navigate to={"/"} />;
   }
 
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
         <h1 className="text-4xl text-center mb-4">Login</h1>
-        <form className="max-w-md mx-auto" onSubmit={handleLoginSubmit}>
-          <input type="email"
-                 placeholder="your@email.com"
-                 value={email}
-                 onChange={ev => setEmail(ev.target.value)} 
-                 required/>
-          <input type="password"
-                 placeholder="password"
-                 value={password}
-                 onChange={ev => setPassword(ev.target.value)} 
-                 required/>
-          <button className="primary">Login</button>
+        <form
+          className="max-w-md mx-auto flex flex-col gap-2 w-[400px]"
+          onSubmit={handleLoginSubmit}
+        >
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={(ev) => setEmail(ev.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(ev) => setPassword(ev.target.value)}
+            required
+          />
+          <button className="primary font-semibold text-center">Login</button>
           <div className="text-center py-2 text-gray-500">
-            Don't have an account yet? <Link className="underline text-black" to={'/register'}>Register now</Link>
+            Don't have an account yet?{" "}
+            <Link className="underline text-black" to={"/register"}>
+              Register now
+            </Link>
           </div>
         </form>
       </div>
