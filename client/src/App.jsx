429d9f309568f546b56ae4b0918030555dfa802c
Luat <you@example.com>
2023-06-28 13:59:00 +0700

    Update recommender page and notification page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,19 @@
-import './App.css'
-import {Route, Routes} from "react-router-dom";
-import IndexPage from "./pages/IndexPage.jsx";
+import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import Layout from "./Layout";
 import RegisterPage from "./pages/RegisterPage";
 import axios from "axios";
-import {UserContextProvider} from "./UserContext";
-import ProfilePage from "./pages/ProfilePage.jsx";
+import { UserContextProvider } from "./UserContext";
 import PlacesPage from "./pages/PlacesPage";
 import PlaceSearchPage from "./pages/PlaceSearchPage";
 import PlacesFormPage from "./pages/PlacesFormPage";
 import PlacePage from "./pages/PlacePage";
 import BookingsPage from "./pages/BookingsPage";
 import BookingPage from "./pages/BookingPage";
+import NotificationsPage from "./pages/NotificationsPage";
+import RecommendationPage from "./pages/RecommendationPage";
+import 'swiper/swiper-bundle.css';
+
 
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
@@ -22,11 +23,15 @@ function App() {
     <UserContextProvider>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<IndexPage />} />
-          <Route path='/search' element={<PlaceSearchPage />} />
+          <Route index element={<RecommendationPage />} />
+          <Route path="/recommendations" element={<RecommendationPage />} />
+          <Route
+            path="/account/notifications"
+            element={<NotificationsPage />}
+          />
+          <Route path="/search" element={<PlaceSearchPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/account" element={<ProfilePage />} />
           <Route path="/account/places" element={<PlacesPage />} />
           <Route path="/account/places/new" element={<PlacesFormPage />} />
           <Route path="/account/places/:id" element={<PlacesFormPage />} />
@@ -37,7 +42,7 @@ function App() {
         </Route>
       </Routes>
     </UserContextProvider>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/PlaceSlider.jsx
@@ -0,0 +1,135 @@
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  StarIcon,
+} from "@heroicons/react/24/solid";
+import { useEffect, useState } from "react";
+import { Controller } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import Image from "../Image";
+import { classNames } from "../common/helpers";
+
+function PlaceSlider({ places = [] }) {
+  const [swiperController, setSwiperController] = useState();
+  const [canNext, setCanNext] = useState(false);
+  const [canPrev, setCanPrev] = useState(false);
+  useEffect(() => {
+    const handleResize = () => {
+      if (swiperController) {
+        setCanPrev(!swiperController?.isBeginning);
+        setCanNext(!swiperController?.isEnd);
+      }
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    document.addEventListener("DOMContentLoaded", handleResize)
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      document.removeEventListener("DOMContentLoaded", handleResize)
+
+    };
+  }, [swiperController]);
+  return (
+    <div className={"relative"}>
+      <Swiper
+        modules={[Controller]}
+        onSwiper={setSwiperController}
+        slidesPerView={10}
+        spaceBetween={48}
+        breakpoints={{
+          320: {
+            slidesPerView: 1,
+          },
+          640: {
+            slidesPerView: 2,
+          },
+          1000: {
+            slidesPerView: 4,
+          },
+          1280: {
+            slidesPerView: 4,
+          },
+        }}
+        onSlideChange={(event) => {
+          setCanPrev(!event?.isBeginning);
+          setCanNext(!event?.isEnd);
+        }}
+      >
+        {places?.map((place, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <div
+                className={classNames(
+                  "flex w-full justify-center transition-all cursor-pointer"
+                )}
+              >
+                <div
+                  className={
+                    "flex w-full flex-col items-center justify-center gap-3"
+                  }
+                >
+                  <Image
+                    className={
+                      "w-full aspect-square object-cover rounded-[12px]"
+                    }
+                    src={place.photos?.[0] || ""}
+                    alt={place.title || ""}
+                    width={100}
+                    height={100}
+                  />
+                  <div className="flex flex-col gap-[2px] w-full">
+                    <div className="flex justify-between gap-1">
+                      <h4 className="text-base font-semibold line-clamp-1">{place.title}, {place.address}</h4>
+                      <span className="text-base flex items-center">
+                        <StarIcon className="w-4 h-4 mr-1" />{" "}
+                        <span className="text-sm">{place.rating?.toFixed(2) || "0.00"}</span>
+                      </span>
+                    </div>
+                    <p className="text-sm text-secondary">
+                      {place.address}
+                    </p>
+                    <p className="text-sm">
+                    <span className="text-md font-semibold">{place.price}$ / </span> <span>night</span>   
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+      {canPrev ? (
+        <button
+          onClick={() => {
+            swiperController?.slidePrev();
+          }}
+          className={`absolute left-[-1%] top-[50%] rounded-full w-7 h-7 bg-white drop-shadow sm:top-[40%] md:left-[-2%] flex justify-center items-center hover:shadow-lg transition-all`}
+          style={{
+            transform: "translateY(-50%)",
+            zIndex: 10,
+          }}
+        >
+          <ChevronLeftIcon className="w-3 h-3" />
+        </button>
+      ) : null}
+      {canNext ? (
+        <button
+          onClick={() => {
+            swiperController?.slideNext();
+          }}
+          className={
+            "absolute right-[-1%] top-[50%] rounded-full  w-7 h-7 bg-white drop-shadow sm:top-[40%] md:right-[-2%] flex justify-center items-center hover:shadow-lg transition-all"
+          }
+          style={{
+            transform: "translateY(-50%)",
+            zIndex: 10,
+          }}
+        >
+          <ChevronRightIcon className="w-3 h-3" />
+        </button>
+      ) : null}
+    </div>
+  );
+}
+
+export default PlaceSlider;
