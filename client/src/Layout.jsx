9dd8a90ba00298abf14fb8f4ecfb380ed5634d3a
thinh205171 <75664882+thinh205171@users.noreply.github.com>
2023-07-05 17:10:15 +0700

    Update Layout.jsx

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 export default function Layout() {
   return (
     <div className=" flex flex-col min-h-screen ">
-      <div className="px-[40px] lg:px-[80px] 2xl:px-[100px] relative z-20">
+      <div className="px-[40px] lg:px-[80px] 2xl:px-[100px]">
         <Header />
       </div>
       <div className="w-full border border-b border-gray-100"></div>
