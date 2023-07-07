e740eac3ce4c0bdc4b09bd4f80a5e9caa0011e85
trung-nc <nguyentien.work410@gmail.com>
2023-07-07 13:03:10 +0700

    Test

--- api/models/Image.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const ImageSchema = new Schema({
-  filename: String,
-  data: String,
-});
-
-module.exports = mongoose.model("Image", ImageSchema);
